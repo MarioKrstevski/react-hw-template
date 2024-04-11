d0f7f499f2aca91c081dd0a1e1400e2387699cee
Mario Krstevski <mariokrstevski@hotmail.com>
2024-04-11 00:51:16 -0400

    First Commit Setup

---
+++ src/Homework/H0-Example/H0.js
@@ -0,0 +1,15 @@
+function Task1() {
+  return <></>;
+}
+function SomeComponent() {
+  return <></>;
+}
+export default function H0(props) {
+  return (
+    <div>
+      H0 works
+      <Task1 />
+      <SomeComponent />
+    </div>
+  );
+}
